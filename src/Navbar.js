0c7c0e8ced6184cb6bb9434eedde318265c9d342
Ankit Madhukar <ankitmadhukar23@gmail.com>
2022-07-29 09:49:22 +0530

    1st Commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,9 @@ const Navbar = () => {
         <nav className="Navbar">
             <h1>BaceFook</h1>
             <div className="links">
-                <a href="/SignUp">SignUp</a>
-                <a href="/SignIn">SignIn</a>
-                <a href="/">DashBoard</a>
+                {/* <a href="/SignUp">SignUp</a>
+                <a href="/SignIn">SignIn</a> */}
+                <a href="#">DashBoard</a>
             </div>
         </nav>
     );
